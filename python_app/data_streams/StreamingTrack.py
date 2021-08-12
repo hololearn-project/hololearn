b8e1ccf744c16f108f891c485b55032a5148aea6
Nathan Ordonez <nathanaxcan@gmail.com>
2021-08-12 11:56:25 +0200

    fixed imports

--- python_app/data_streams/StreamingTrack.py
+++ python_app/data_streams/StreamingTrack.py
@@ -1,6 +1,6 @@
 from data_streams.vstreamer import LidarVideoTrack, LidarDepthTrack, LidarVideoSkeletonTrack, WebcamVideoTrack  # noqa: E501
 from cameras.cameras import camera_wrapper as camera_obj
-from cameras.camera_intel_skeleton import intelcamSkeleton
+# from cameras.camera_intel_skeleton import intelcamSkeleton
 
 
 class StreamingTrack:
@@ -10,9 +10,10 @@ class StreamingTrack:
 
         self.depthTrack = LidarDepthTrack(self.cam)
 
-        if isinstance(self.cam, intelcamSkeleton):
-            self.videoTrack = LidarVideoSkeletonTrack(self.cam)
-        else:
-            self.videoTrack = LidarVideoTrack(self.cam)
+        # if isinstance(self.cam, intelcamSkeleton):
+        #     self.videoTrack = LidarVideoSkeletonTrack(self.cam)
+        # else:
+        #     self.videoTrack = LidarVideoTrack(self.cam)
+        self.videoTrack = LidarVideoTrack(self.cam)
 
         self.webcamTrack = WebcamVideoTrack()
