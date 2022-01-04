a6845e394b1ba96ae40bd507603ae2b7626a3336
LMMilliken <58855099+LMMilliken@users.noreply.github.com>
2022-01-04 14:13:02 +0100

    fixed encoding type of bgrimage

--- python_app/data_streams/vstreamer.py
+++ python_app/data_streams/vstreamer.py
@@ -132,7 +132,7 @@ class LidarBGRTrack(VideoStreamTrack):
         pts, time_base = await self.next_timestamp()
         cvframe = self.cam.get_frame_bgr()
         cvframe = np.array(cvframe, dtype=np.uint8)
-        frame = VideoFrame.from_ndarray(cvframe, format='bgr')
+        frame = VideoFrame.from_ndarray(cvframe, format='rgb24')
         frame.pts = pts
         frame.time_base = time_base
         return frame
