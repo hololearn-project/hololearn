22137aa1d553b5402691f6fa7ba4a96f4632fd39
ottedam <ottevandam@gmail.com>
2022-01-11 21:10:40 +0100

    server test

--- node_server/app.js
+++ node_server/app.js
@@ -1148,7 +1148,19 @@ io.sockets.on('connection', (socket) => {
   });
 });
 server.listen(port, () => console.log(`Server is running on port ${port}`));
-console.log(server);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(10000);
+  console.log(server);
+
+  // Sleep in loop
+}
+
+demo();
 
 // server.listen(port, () => console.log(`Server is running on port ${port}`));
 
