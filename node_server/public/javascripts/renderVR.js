ba7f81e7843013c059d6238599c5ba23959fd8b6
LMMilliken <58855099+LMMilliken@users.noreply.github.com>
2022-03-24 16:52:45 +0100

    gggv

--- node_server/public/javascripts/renderVR.js
+++ node_server/public/javascripts/renderVR.js
@@ -39,13 +39,11 @@ function moveObjectToVR(object) {
   if (isBasically(object.position.x, 0) && isBasically(object.position.y, 3) && isBasically(object.position.z, 0)) {
     return;
   }
-  object.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+  object.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.PI - rotationNow);
 
   object.position.x = object.position.x + a;
   object.position.y = object.position.y - b + 1.6;
   object.position.z = object.position.z + c;
-
-  object.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -rotationNow);
 }
 
 /**
