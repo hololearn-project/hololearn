91e3cb27d8534f9772bf0428d4d340c883c25542
ottedam <ottevandam@gmail.com>
2022-07-06 14:28:18 +0200

    Revert changes for test

--- node_server/public/javascripts/threejs-scripts/modelling.js
+++ node_server/public/javascripts/threejs-scripts/modelling.js
@@ -615,9 +615,8 @@ function modelFromIndexes(dctx) {
  * performs backgorund removal with sensor flow.
  * @param {HTMLCanvasElement} ctx The canvas containing the picture image
  */
-async function model2DNoDepth() {
+function model2DNoDepth() {
   const twod = document.getElementById('2d');
-  await new Promise(r => setTimeout(r, 6000));
   ctx.drawImage(document.getElementById('teacherRecording'), 0, 0, imgWidth, imgLength);
 
   ctxFlatCanvas.putImageData(removeBlackBackgroundTeacherRecording(ctx.getImageData(0, 0, imgWidth, imgLength)), 0, 0);
